d77e6558b0825d65809c25cc0aab9b1594025a6f
GitMats85 <107111236+GitMats85@users.noreply.github.com>
2024-03-15 22:47:04 +0100

    mer fix

--- DIG01-EXEMPEL/Assets/Scrips/ChangeLevel.cs
+++ DIG01-EXEMPEL/Assets/Scrips/ChangeLevel.cs
@@ -6,16 +6,12 @@ using UnityEngine.SceneManagement;
 public class ChangeLevel : MonoBehaviour
 {
     [SerializeField] string goToLevel;
-    [SerializeField] float newPosX;
-    [SerializeField] float newPosY;
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             FindObjectOfType<ScenePersist>().ResetScenePersist();
             SceneManager.LoadScene(goToLevel);
-            FindObjectOfType<PlayerMovement>().SetStartPos(newPosX,newPosY);
         }
     }
 }
