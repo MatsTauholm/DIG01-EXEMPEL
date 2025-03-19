76b2ed9265c130439876d650c72d0e809f918b5a
MatsTauholm <mats.tauholm@lbs.se>
2025-03-19 15:13:11 +0100

    Collision update

--- DIG01-EXEMPEL/Assets/Scrips/Physics Overlap/Bomb.cs
+++ DIG01-EXEMPEL/Assets/Scrips/Physics Overlap/Bomb.cs
@@ -2,25 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Bomb : MonoBehaviour
 {
     public float explosionRadius = 5f;
 
-    void OnFire()
+    void Update()
     {
-        DestroyEnemies();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DestroyEnemies();
+        } 
     }
 
     void DestroyEnemies()
     {
-        LayerMask enemyLayer = LayerMask.GetMask("Enemies"); //Referens till Enemies layer
+        LayerMask enemyLayer = LayerMask.GetMask("Enemies"); //Referens till det layer som alla enemies ligger i
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, explosionRadius, enemyLayer); //Kollar efter alla colliders inom cirkeln och lägger till dessa i en array
 
         foreach (Collider2D enemy in hitEnemies) //Kolla igenom hela array:n       
         {
-             Destroy(enemy.gameObject); //Förstör objektet
+             Destroy(enemy.gameObject); //Förstör de objektet som hittas
         }
 
     }
